9abd92c59a2a8ff08b4eddd21daf665c457da243
Tony Yeung <mr.tyeung@gmail.com>
2024-08-22 20:43:38 -0700

    refactor: added ability to show weather forecast for the patient's home zip code

--- src/pages/PatientPage.tsx
+++ src/pages/PatientPage.tsx
@@ -1,8 +1,8 @@
-import { Loader, Tabs } from '@mantine/core';
+import { Loader, Tabs, Title } from '@mantine/core';
 import { getReferenceString } from '@medplum/core';
 import { Patient } from '@medplum/fhirtypes';
 import { useResource } from '@medplum/react';
-import { Fragment } from 'react';
+import { Fragment, useEffect, useState } from 'react';
 import { Outlet, useNavigate, useParams } from 'react-router-dom';
 import { PatientHeader } from './PatientHeader';
 
@@ -10,6 +10,70 @@ export function PatientPage(): JSX.Element {
   const navigate = useNavigate();
   const { id } = useParams();
   const patient = useResource<Patient>({ reference: `Patient/${id}` });
+
+  interface LocationInfo {
+    zip: string;
+    name: string;
+    lat: number;
+    lon: number;
+    country: string;
+  }
+  interface WeatherInfo {
+    lat: number;
+    lon: number;
+    tz: string;
+    date: string;
+    units: string;
+    weather_overview: string;
+  }
+
+  const [postalCode, setPostalCode] = useState<string | undefined>();
+  const [location, setLocation] = useState<LocationInfo | undefined>();
+  const [weatherInfo, setWeatherInfo] = useState<WeatherInfo | undefined>();
+
+  const OPEN_WEATHER_API_KEY = import.meta.env.VITE_OPEN_WEATHER_API_KEY;
+
+  useEffect(() => {
+    const fetchLocation = (patient: Patient | undefined): void => {
+      if (patient) {
+        const { address } = patient
+
+        if (address?.length) {
+          setPostalCode(address[0].postalCode);
+        }
+
+      }
+    };
+
+    const fetchWeather = async (patient: Patient | undefined, openWeatherApiKey: string): Promise<void> => {
+      fetchLocation(patient);
+
+      if (postalCode) {
+        const DIRECT_GEOCODE_BY_ZIP = `http://api.openweathermap.org/geo/1.0/zip?zip=${postalCode},US&appid=${openWeatherApiKey}`;
+
+        const resp = await fetch(DIRECT_GEOCODE_BY_ZIP)
+        if (resp.ok) {
+          const data = await resp.json();
+          setLocation(data);
+        }
+
+        if (location) {
+          const { lat, lon } = location;
+          const WEATHER_OVERVIEW_URL = `https://api.openweathermap.org/data/3.0/onecall/overview?lat=${lat}&lon=${lon}&appid=${openWeatherApiKey}`
+          const result = await fetch(WEATHER_OVERVIEW_URL);
+          if (result.ok) {
+            const weather = await result.json();
+            setWeatherInfo(weather);
+          } else {
+            console.log(result.statusText);
+          }
+        }
+      }
+    };
+
+    fetchWeather(patient, OPEN_WEATHER_API_KEY)
+  }, [patient, postalCode]);
+
   if (!patient) {
     return <Loader />;
   }
@@ -17,6 +81,9 @@ export function PatientPage(): JSX.Element {
   return (
     <Fragment key={getReferenceString(patient)}>
       <PatientHeader patient={patient} />
+      <Title>
+        FORECAST for {location?.zip}: {weatherInfo ? weatherInfo.weather_overview.substring(weatherInfo.weather_overview.lastIndexOf('overall')) : ''}
+      </Title>
       <Tabs onChange={(t) => navigate(`./${t}`)}>
         <Tabs.List bg="white">
           <Tabs.Tab value="overview">Overview</Tabs.Tab>
